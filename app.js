d56440c22275c998c8c7a87cf1a7b7dc6429b4b1
Sushant Dasuri <sushantdasuri@IN-C02F54APMD6M.local>
2025-05-29 16:09:07 +0530

    Conceptual Aside: POJOs

--- app.js
+++ app.js
@@ -1,9 +1,70 @@
-function countDown(num) {
-    console.log(num);
-
-    if(num === 0) return;
-
-    countDown(num - 1);
+let markup = {
+    type: 'article',
+    children: [
+        {
+            type: 'h2',
+            children: [
+                {
+                    type: 'text',
+                    value: 'Counter'
+                }
+            ]
+        },
+        {
+            type: 'p',
+            children: [
+                {
+                    type: 'text',
+                    value: 'Counter '
+                },
+                {
+                    type: 'strong',
+                    children: [
+                        {
+                            type: 'em',
+                            children: [
+                                {
+                                    type: 'text',
+                                    value: '1'
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    type: 'text',
+                    value: ' times'
+                }
+            ]
+        },
+        {
+            type: 'button',
+            children: [
+                {
+                    type: 'text',
+                    value: 'Click me'
+                }
+            ]
+        }
+    ]
 }
 
-countDown(5);
+console.log(markup);
+
+const main = document.getElementById("app");
+
+function addElements(pojoElement, parentDOMNode) {
+    let newDOMNode = pojoElement.type === 'text' ? document.createTextNode(pojoElement.value) : document.createElement(pojoElement.type);
+
+    if(pojoElement.children) {
+        pojoElement.children.forEach( (child) => {
+            addElements(child, newDOMNode);
+        });
+    }
+
+    console.log(newDOMNode);
+    parentDOMNode.appendChild(newDOMNode);
+
+}
+
+addElements(markup, main);
