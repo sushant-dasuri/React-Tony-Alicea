169486c1a6a78f3949a64f042c60e8d097f16fb8
Sushant Dasuri <sushantdasuri@IN-C02F54APMD6M.local>
2025-05-29 20:24:20 +0530

    Components

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const root = ReactDOM.createRoot(rootNode);
 root.render(React.createElement(App));
 
 function App() {
+    return React.createElement("section", null,
+           React.createElement("h1", null, "Counters"),
+           React.createElement("section", null,
+            React.createElement(Counter, null))
+    )
+}
+
+function Counter() {
     return (
         React.createElement("article", null,
             React.createElement("h2", null, "Counter"),
