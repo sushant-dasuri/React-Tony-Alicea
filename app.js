3cf5a6ce1eb1cf5eb2223ce53f5dc19eb07e114b
Sushant Dasuri <sushantdasuri@IN-C02F54APMD6M.local>
2025-06-17 20:09:15 +0530

    React Element and JSX

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
-const rootNode = document.getElementById("app");
-console.log(rootNode);
-let counterName = "One";
+const rootNode = document.getElementById('app');
 const root = ReactDOM.createRoot(rootNode);
 root.render(React.createElement(App));
 
 function App() {
-    return React.createElement("section", null,
-           React.createElement("h1", null, "Counters"),
-           React.createElement("section", null,
-            React.createElement(Counter, null))
-    )
-}
-
-function Counter({name}) {
     return (
-        React.createElement("article", null,
-            React.createElement("h2", null, "Counter"),
-            React.createElement("p", null, "You clicked 1 times"),
-            React.createElement("button", {className: "button", name: "Sushant"}, "Click me!"),
-        )
+        <section>
+            <h1>Counters</h1>
+            <section>
+                <Counter />
+            </section>
+        </section>
     );
 }
 
-
-
-
-
+function Counter(props) {
+    return (
+        <article>
+            <h2>Counter</h2>
+            <p>You clicked 1 times</p>
+            <button className="button">
+                Click me
+            </button>
+        </article>
+    );
+}
