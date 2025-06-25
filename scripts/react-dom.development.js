a65a4f794fa366efcc6310f457336dcd715e73c0
Sushant Dasuri <sushant.dasuri@oliver.agency>
2025-06-25 15:23:27 +0530

    Fiber and custom execution context

--- scripts/react-dom.development.js
+++ scripts/react-dom.development.js
@@ -26892,6 +26892,7 @@
 
 
       requestPaint();
+      console.log("Execution Context: " + executionContext);
       executionContext = prevExecutionContext; // Reset the priority to the previous non-sync value.
 
       setCurrentUpdatePriority(previousPriority);
