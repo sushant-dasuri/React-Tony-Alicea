42f898d6a5e940c6a7ee0508250503340acb5de6
Sushant Dasuri <sushant.dasuri@oliver.agency>
2025-07-03 20:29:58 +0530

    Immutable State

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
-const person1 = {
-    firstname: 'Tony',
-    lastname: 'Alicea',
-    course: {
-        name: 'Understanding React'
-    }
+const rootNode = document.getElementById('app');
+const root = ReactDOM.createRoot(rootNode);
+let counterName = "One";
+root.render(<App />);
+
+function App() {
+  
+    return (
+        <>
+            <h1>Counters</h1>
+            <section>
+                <Counter name="One" />
+                 <Counter name="Two" />
+            </section>
+        </>
+    );
 }
 
-const person2 = {
-    firstname: 'Tony',
-    lastname: 'Alicea',
-    course: {
-        name: 'Understanding React'
+function Counter(props) {
+
+    const [numOfClicks, setNumOfClicks] = React.useState({total: 0});
+
+    function wrongIncrementCounter() {
+        numOfClicks.total = numOfClicks.total + 1;
+        setNumOfClicks(numOfClicks);
+    }  
+
+    function incrementCounter() {
+       setNumOfClicks({...numOfClicks, total: numOfClicks.total + 1})
+       
     }
+    return (
+        <article>
+            <h2>Counter {props.name}</h2>
+            <p>You clicked {numOfClicks.total} times</p>
+            <p>
+                <button className="button" onClick={wrongIncrementCounter}>
+                    Click wont work
+                </button>
+            </p>
+              <p>
+                <button className="button" onClick={incrementCounter}>
+                    Click me
+                </button>
+            </p>
+        </article>
+    );
 }
-
-console.log(Object.is(person1, person2));
-const objectIs = Object.is;
-
-function shallowEqual(objA, objB) {
-    if (objectIs(objA, objB)) {
-      return true;
-    }
-
-    if (typeof objA !== 'object' || objA === null || typeof objB !== 'object' || objB === null) {
-      return false;
-    }
-
-    var keysA = Object.keys(objA);
-    var keysB = Object.keys(objB);
-
-    if (keysA.length !== keysB.length) {
-      return false;
-    } // Test for A's keys different from B.
-
-
-    for (var i = 0; i < keysA.length; i++) {
-      var currentKey = keysA[i];
-
-      if (!hasOwnProperty.call(objB, currentKey) || !objectIs(objA[currentKey], objB[currentKey])) {
-        return false;
-      }
-    }
-
-    return true;
-}
-
-console.log(shallowEqual(person1, person2)); // false because course points to a different object in each case
