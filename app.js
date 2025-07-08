455b80fb92d822a81767d1c23cfafd3400a19198
Sushant Dasuri <sushant.dasuri@oliver.agency>
2025-07-08 20:03:25 +0530

    Adding your own effects

--- app.js
+++ app.js
@@ -20,24 +20,20 @@ function Counter(props) {
 
     const [numOfClicks, setNumOfClicks] = React.useState({total: 0});
 
-    function wrongIncrementCounter() {
-        numOfClicks.total = numOfClicks.total + 1;
-        setNumOfClicks(numOfClicks);
-    }  
-
     function incrementCounter() {
        setNumOfClicks({...numOfClicks, total: numOfClicks.total + 1})
        
     }
+
+    React.useEffect(() => {
+
+        console.log("In UseEffect");
+        document.title = "Counter : " + numOfClicks.total;
+    });
     return (
         <article>
             <h2>Counter {props.name}</h2>
             <p>You clicked {numOfClicks.total} times</p>
-            <p>
-                <button className="button" onClick={wrongIncrementCounter}>
-                    Click wont work
-                </button>
-            </p>
               <p>
                 <button className="button" onClick={incrementCounter}>
                     Click me
