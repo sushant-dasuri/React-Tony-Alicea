32b3820b15668b6999c296eee7102748a9dad160
Sushant Dasuri <sushant.dasuri@oliver.agency>
2025-07-09 22:08:49 +0530

    Fetching Data...or not

--- app.js
+++ app.js
@@ -1,64 +1,43 @@
 const rootNode = document.getElementById('app');
 const root = ReactDOM.createRoot(rootNode);
-let counterName = "One";
 root.render(<App />);
 
-function App() {
-
-    const [swapCounter, setSwapCounter] = React.useState(false);
-
-    function handleSwap() {
-        setSwapCounter(!swapCounter);
-    }
-
-    let counterOne = null;
-    if(!swapCounter) {
-        counterOne = <Counter name="One" />;
-    }
-  
-    return (
-        <>
-            <h1>Counters</h1>
-            <section>
-                {counterOne}
-               { swapCounter ? <Counter name="Two" /> :  null}
-            </section>
-            <p>
-                <button className="button" onClick={handleSwap}>
-                    Swap Counters
-                </button>
-            </p>
-        </>
-    );
+async function fetchBio(person) {
+    const delay = person === 'Bob' ? 2000 : 200;
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('This is ' + person + '’s bio.');
+        }, delay);
+    })
 }
 
-function Counter(props) {
-
-    const [numOfClicks, setNumOfClicks] = React.useState({total: 0});
-
-    function incrementCounter() {
-       setNumOfClicks({...numOfClicks, total: numOfClicks.total + 1})
-       
-    }
-
+function App() {
+    const [person, setPerson] = React.useState('Alice');
+    const [bio, setBio] = React.useState(null);
     React.useEffect(() => {
-
-        console.log("In UseEffect " + props.name);
-        document.title = "Counter : " + numOfClicks.total;
-
-        return() => {
-            console.log("Unmounting " + props.name);
+        let ignore = false;
+        setBio(null);
+        fetchBio(person).then(result => {
+            if (!ignore) {
+                setBio(result);
+            }
+        });
+        return () => {
+            ignore = true;
         }
-    }, [numOfClicks.total]);
+    }, [person]);
+
     return (
-        <article>
-            <h2>Counter {props.name}</h2>
-            <p>You clicked {numOfClicks.total} times</p>
-              <p>
-                <button className="button" onClick={incrementCounter}>
-                    Click me
-                </button>
-            </p>
-        </article>
+        <>
+            <select value={person} onChange={e => {
+                setPerson(e.target.value);
+            }}>
+                <option value="Alice">Alice</option>
+                <option value="Bob">Bob</option>
+                <option value="Taylor">Taylor</option>
+            </select>
+            <hr />
+            <p><i>{bio ?? 'Loading...'}</i></p>
+        </>
     );
 }
