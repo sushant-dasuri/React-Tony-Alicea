2bdbeddca13136237850a78e3bd63944c802c636
Sushant Dasuri <sushant.dasuri@oliver.agency>
2025-07-10 11:37:39 +0530

    Conceptual Aside: Stale Closures

--- app.js
+++ app.js
@@ -1,43 +1,22 @@
-const rootNode = document.getElementById('app');
-const root = ReactDOM.createRoot(rootNode);
-root.render(<App />);
+function createCounter(incBy) {
+    let value = 0;
 
-async function fetchBio(person) {
-    const delay = person === 'Bob' ? 2000 : 200;
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve('This is ' + person + '’s bio.');
-        }, delay);
-    })
+    function increment() {
+        value += incBy;
+        console.log(value);
+    }
+
+    const message = `Current value is ${value}`;
+    function log() {
+        console.log(message);
+    }
+
+    return [increment, log];
 }
 
-function App() {
-    const [person, setPerson] = React.useState('Alice');
-    const [bio, setBio] = React.useState(null);
-    React.useEffect(() => {
-        let ignore = false;
-        setBio(null);
-        fetchBio(person).then(result => {
-            if (!ignore) {
-                setBio(result);
-            }
-        });
-        return () => {
-            ignore = true;
-        }
-    }, [person]);
+const [increment, log] = createCounter(1);
+increment();
+increment();
+increment();
 
-    return (
-        <>
-            <select value={person} onChange={e => {
-                setPerson(e.target.value);
-            }}>
-                <option value="Alice">Alice</option>
-                <option value="Bob">Bob</option>
-                <option value="Taylor">Taylor</option>
-            </select>
-            <hr />
-            <p><i>{bio ?? 'Loading...'}</i></p>
-        </>
-    );
-}
+log();
