e654cb8cb90f2c6266b611a6246ae2d99f7c138d
Sushant Dasuri <sushant.dasuri@oliver.agency>
2025-09-04 16:14:50 +0530

    Controlled Inputs

--- vite_sandbox/src/components/AddCounter.jsx
+++ vite_sandbox/src/components/AddCounter.jsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
+
 export function AddCounter() {
+
+    const [counterName, setCounterName] = useState('');
+    const [startingValue, setStartingValue] = useState(1);
+
+
     return (
         <>
-            <h2>Add Counter</h2>
+            <h2>Add {counterName}</h2>
             <p>
                 <label htmlFor="counterName">Name:</label>
-                <input type="text" id ="counterName" />
+                <input type="text" id ="counterName" value={counterName} onChange={(event)=> {setCounterName(event.target.value)}} />
             </p>
             <p>
                 <label htmlFor="startingValue">Starting Value</label>
-                <input defaultValue="1" type="number" id="startingValue" />
+                <input defaultValue="1" type="number" id="startingValue" value={startingValue} onChange={(event)=> {setStartingValue(event.target.value)}} />
             </p>
         </>
     )
