b96ca043d23e7fbf95cd7c724d176fcd0ad0b41a
Sushant Dasuri <sushant.dasuri@oliver.agency>
2025-09-05 20:59:47 +0530

    useDebugValue

--- vite_sandbox/src/hooks/useDocumentTitle.js
+++ vite_sandbox/src/hooks/useDocumentTitle.js
@@ -1,6 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useDebugValue } from "react";
 
 export function useDocumentTitle (title) {
+
+    useDebugValue(title);
    return useEffect(() => {
     const originalTitle = document.title;
     document.title = title;
