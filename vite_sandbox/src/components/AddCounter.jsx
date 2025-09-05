5158693b8d39c58b14ae78195d1831d0fd25d9c0
Sushant Dasuri <sushant.dasuri@oliver.agency>
2025-09-05 19:48:34 +0530

    textarea, select and more

--- vite_sandbox/src/components/AddCounter.jsx
+++ vite_sandbox/src/components/AddCounter.jsx
@@ -1,30 +1,47 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { CounterDispatchContext } from "../contexts/context";
 
 export function AddCounter() {
 
-    const [counterName, setCounterName] = useState('');
+    const counterDispatch = useContext(CounterDispatchContext);
+    const [counterShortName, setCounterShortName] = useState('');
+    const [counterLongName, setCounterLongName] = useState('');
+    const [tab, setTab] = useState(1);
     const [startingValue, setStartingValue] = useState(1);
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(counterName);
-        console.log(startingValue);
-      
-        const form = event.target;
-        const formData = new FormData(form);
-        console.log(...formData);
 
-        const formJson = Object.fromEntries(formData.entries());
-        console.log(formJson);
+        counterDispatch({
+            type: 'add',
+            data: {
+                shortName: counterShortName,
+                longName: counterLongName,
+                tab: parseInt(tab),
+                startingValue: parseInt(startingValue)
+            }
+        })
+       
     }
 
     return (
         <>
             <form method="post" onSubmit={handleSubmit}>
-            <h2>Add {counterName}</h2>
+            <h2>Add {counterShortName}</h2>
             <p>
-                <label htmlFor="counterName">Name:</label>
-                <input type="text" name="counterName" id ="counterName" value={counterName} onChange={(event)=> {setCounterName(event.target.value)}} />
+                <label htmlFor="counterShortName">Short Name:</label>
+                <input type="text" name="counterShortName" id ="counterShortName" value={counterShortName} onChange={(event)=> {setCounterShortName(event.target.value)}} />
+            </p>
+             <p>
+                <label htmlFor="counterLongName">Long Name:</label>
+                <textarea name="counterLongName" id ="counterLongName" value={counterLongName} onChange={(event)=> {setCounterLongName(event.target.value)}}></textarea>
+            </p>
+            <p>
+                <label htmlFor="tab">Tab</label>
+                <select name="tab" id="tab" value={tab} onChange={(event)=> {setTab(event.target.value)}}>
+                    <option value={1}>Tab 1</option>
+                    <option value={2}>Tab 2</option>
+                </select>
             </p>
             <p>
                 <label htmlFor="startingValue">Starting Value</label>
--- vite_sandbox/src/reducers/counterReducer.js
+++ vite_sandbox/src/reducers/counterReducer.js
@@ -1,3 +1,5 @@
+import { CounterObj } from "../models/CounterObj";
+
 export function counterReducer(counterData, action) {
     switch (action.type) {
         case 'increment' : {
@@ -20,6 +22,12 @@ export function counterReducer(counterData, action) {
                     return counter;
                 }
             })
+        }
+
+        case 'add' : {
+            const newCounter = new CounterObj(counterData[counterData.length -1] + 1, {shortName: action.data.shortName, longName: action.data.longName}, action.data.tab, action.data.startingValue);
+            console.log(newCounter);
+            return [...counterData, newCounter];
         }
 
         default: {
